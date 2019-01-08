4ae76ebff3e960d21ac5dd48110249a05ab7a181
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2019-01-08 17:50:17 +0400

    Update babel configs

--- babel.config.js
+++ babel.config.js
@@ -4,17 +4,9 @@ module.exports = {
         '@babel/preset-react',
         '@babel/preset-flow',
     ],
-    env: {
-        test: {
-            presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-            ],
-            plugins: [
-                '@babel/plugin-proposal-class-properties',
-                'transform-es2015-modules-commonjs',
-                'babel-plugin-dynamic-import-node',
-            ],
-        },
-    },
+    plugins: [
+        '@babel/plugin-proposal-class-properties',
+        'transform-es2015-modules-commonjs',
+        'babel-plugin-dynamic-import-node',
+    ],
 };
