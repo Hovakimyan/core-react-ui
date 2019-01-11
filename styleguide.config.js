76df2209be67bc193c2001f31d16305635d92baa
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2019-01-11 18:51:59 +0400

    Update port of config

--- styleguide.config.js
+++ styleguide.config.js
@@ -10,6 +10,7 @@ module.exports = {
     exampleMode: 'expand',
     usageMode: 'expand',
     pagePerSection: true,
+    serverPort: 3000,
     theme: {
         baseBackground: '#fdfdfc',
         link: '#274e75',
