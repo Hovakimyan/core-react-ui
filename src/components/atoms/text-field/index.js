990846483bab31a82d05fc15a4953f45b660171f
EdwardStepanian <edward.stepanian.se@gmail.com>
2019-01-15 16:46:11 +0400

    Make textfield functional component

--- src/components/atoms/text-field/index.js
+++ src/components/atoms/text-field/index.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const styledTextField = styled.textarea`
+const StyledTextField = styled.textarea`
     display: flex;
 `;
 
-export default class TextField extends React.PureComponent {
-    state = {
-        value: '',
-    };
-    
-    handleChange = (state) => {
-        this.setState({
-            value: state.value,
-        });
-    };
-    
-    render() {
-        return (
-            <styledTextField
-                value={this.state.value}
-                onChange={this.handleChange}
-            />
-        );
-    }
+export default function TextField(props){
+    return (
+        <StyledTextField
+            {...props}
+        />
+    );
 }
